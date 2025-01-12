77801cc6dc75063c6b07ea8729c8cf64d54e8a14
Chirila Tudor <tchirila2712@gmail.com>
2025-01-12 21:05:04 +0200

    Initial commit

---
+++ src/main/java/ro/chirila/ExpenseEase/ExpenseEaseApplication.java
@@ -0,0 +1,13 @@
+package ro.chirila.ExpenseEase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseEaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseEaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/chirila/ExpenseEase/ExpenseEaseApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.chirila.ExpenseEase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseEaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
